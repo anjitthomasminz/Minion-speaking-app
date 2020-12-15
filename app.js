0e682ca5a766ec353cb1ddb3ced73ed63f68ec27
MrATM <72404795+anjitthomasminz@users.noreply.github.com>
2020-12-15 17:31:25 +0530

    added: input function in js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var btnSelector = document.querySelector("#btn-translate");
 
+var txtInput = document.querySelector("#text-hold");
+
 
     
 function clickHandler(){
     console.log("clicked!");
+    console.log("input", txtInput.value);
 }
 
 btnSelector.addEventListener("click", clickHandler)
